04e410694e005467a64d9c3e3b50a89307d724c7
Billy Mathison <wlmathison@gmail.com>
2019-03-18 14:54:01 -0500

    adding fetch call to pull from dark sky api, function to input its response into postToDom, and calling the function within postToDom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,1 @@
-const displayContainer = document.getElementById("display-container");
-
-const postToDOM = (park) => {
-    const parkName = park.name;
-    const stateName = park.state;
-    const visitedStatus = park.visited;
-    
-    let displayHTML = `<h3>${parkName}</h3>
-    <p>${stateName}</p>`;
-    
-    let newParkArticle = document.createElement("article");
-    newParkArticle.classList = visitedStatus;
-    newParkArticle.innerHTML = displayHTML;
-    displayContainer.appendChild(newParkArticle)
-}
-
 getParksList()
-
-
